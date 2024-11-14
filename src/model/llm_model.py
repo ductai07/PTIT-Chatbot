66b89fdbe792baf7e49fa8114ef90bc6e7ea712b
Trần Đức Tài <163676773+ductai07@users.noreply.github.com>
2024-11-14 20:27:33 +0700

    Update llm_model.py

--- src/model/llm_model.py
+++ src/model/llm_model.py
@@ -4,6 +4,6 @@ def llm_model():
     llm = GoogleGenerativeAI(model ="gemini-1.5-flash",
         temperature=0,
         verbose=True,
-        google_api_key="AIzaSyAUGwXAao--Npvu4YPsBcyw0NJMOAeteqY"
+        google_api_key="da che"
     )
     return llm
